f131dcf58424bab32b63f84176f1c6ebba785fd9
Jean-Marc Fontaine <jmf@spacelift.io>
2023-11-22 13:11:06 -0500

    Add customizations

---
+++ custom/exporters/terraform.py
@@ -0,0 +1,48 @@
+import logging
+
+from spacemk.exporters.terraform import TerraformExporter as BaseTerraformExporter
+
+
+class TerraformExporter(BaseTerraformExporter):
+    def _drop_aws_access_keys(self, data: dict) -> dict:
+        purged_workspace_variables = []
+
+        for variable in data.get("workspace_variables"):
+            variable_name = variable.get("attributes.key")
+            if variable_name in ["AWS_ACCESS_KEY_ID", "AWS_SECRET_ACCESS_KEY"]:
+                logging.debug(f"Dropping '{variable_name}' workspace env var")
+            else:
+                purged_workspace_variables.append(variable)
+
+        data["workspace_variables"] = purged_workspace_variables
+
+        return data
+
+    def _enrich_workspace_variable_data(self, data: dict) -> dict:
+        logging.warning("Skipping sensitive data extraction for the time being")
+
+        return data
+
+    def _filter_data(self, data: dict) -> dict:
+        data = self._drop_aws_access_keys(data)
+
+        return data
+
+    def _map_data(self, data: dict) -> dict:
+        data = BaseTerraformExporter._map_data(self, data)
+        data = self._materialize_terraform_latest_version(data)
+
+        return data
+
+    def _materialize_terraform_latest_version(self, data: dict) -> dict:
+        updated_stacks = []
+
+        for stack in data.get("stacks"):
+            if stack.get("terraform.version") == "latest":
+                stack["terraform.version"] = "1.5.7"
+
+            updated_stacks.append(stack)
+
+        data["stacks"] = updated_stacks
+
+        return data
